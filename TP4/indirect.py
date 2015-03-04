67b6138143fab6a3dd43547661fb616bfa3d8c6a
Nathan Tutard <ntutard@emi.u-bordeaux1.fr>
2015-03-04 16:19:19 +0100

    correction

--- TP4/indirect.py
+++ TP4/indirect.py
@@ -1,59 +1,51 @@
 # -*- coding: utf-8 -*-
-from math import cos, sin, acos, asin, tan, atan
+from math import cos, sin, acos, asin, tan, atan, atan2
 import math
 
-def leg_id( x ,y ,z ,L1 = 51,L2 = 63.7, L3 =93, alpha=None, beta=None):
+# Cinématique indirecte
+# Auteurs : Nathan Tutard - Louis Douriez - Florian Kauder
 
+def leg_ik( x ,y ,z ,L1 = 51,L2 = 63.7, L3 =93, alpha=None, beta=None):
+    ## Calcul des coefficients d'applications au réel
     if alpha == None:
         alpha = asin(22.5/L2)
     if beta == None:
-        beta = asin(8.2/L3) - math.pi/2
+        beta = asin(8.2/L3)
 
+    ## Calcul des variables "utiles"    
+    Lproj = math.sqrt(x*x + y*y)
+    d13 = Lproj-L1
+    d = math.sqrt(z*z+d13*d13)
+        
+    ## Calcul de T1 : simple tangeante
     if x==0:
         if y>0 :
             T1=math.pi/2
         elif y==0:
-            T1 = 0
-        else :
+            T1=0
+        else:
             T1 = -math.pi/2
     else :
-        T1 = atan(y/x)
-    
-    Lproj = math.sqrt(x*x + y*y)
+        T1 = atan2(y,x)
 
-    if x >= 0:
-        d13 = Lproj-L1
-    else:
-        d13 = Lproj+L1
-        z=abs(z)
-
-    d = math.sqrt(z*z+d13*d13)
-
+    ## Calcul de T2
+    # Calcul de a : On prend en compte les cas extrêmes
     if d13==0:
         if z>0 :
-            a=math.pi/2
+                a=math.pi/2
         elif z== 0:
-            a = 0
+                a = 0
         else :
-            a = -math.pi/2
+                a = -math.pi/2
     else :
         a = atan(z/d13)
 
-    b = acos( (L2*L2 + d*d - (L3*L3) ) / ( 2 * L2 * d ) )
-    bsecond = -b
+    # Calcul de b via Al-kashi
+    b = acos((L2*L2 + d*d - (L3*L3))/(2 * L2 * d))
+    T2 = a + b
 
-    T2 = a +b + alpha
-    T2second =  a + bsecond + alpha
-
-    e=acos ( (L2*L2 + L3*L3 - d*d ) / (2 * L2 * L3) )
-    esecond = -e
-
-    T3 = -(math.pi - e +alpha +beta)
-    T3second = -(math.pi - esecond +alpha +beta)
-
-    if(abs(a+alpha)>abs(b)):
-        return [ math.degrees(T1), math.degrees(T2), math.degrees(T3)]
-    else:
-        return  [math.degrees(T1),math.degrees(T2second), math.degrees(T3second)]
+    ## Calcul de T3 = PI - e (et e calculé via Al-kashi)
+    e = acos((L2*L2 + L3*L3 - d*d)/(2 * L2 * L3))
+    T3 = math.pi - e
     
-   
+    return [math.degrees(T1),-math.degrees(T2+alpha),-math.degrees(T3-math.pi/2+alpha+beta)]
