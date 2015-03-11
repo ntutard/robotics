4983531d4ec4a351c85244e5c350c909660e92a9
Florian Kauder <flokauder@emi.u-bordeaux1.fr>
2015-03-11 15:46:29 +0100

    Add works files for TP2

--- TP2/pypot_example.py
+++ TP2/pypot_example.py
@@ -1,8 +1,12 @@
 import itertools
 import time
 import numpy
+import json
 import pypot.dynamixel
 import pypot.dynamixel.conversion
+import pypot.robot.robot
+from pypot.dynamixel import autodetect_robot
+from pypot.robot import from_json
 from math import cos, sin, acos, asin
 import math
 
@@ -33,7 +37,7 @@ def initialisation(ids):
                 inited = True
                 
     dxl_io.disable_torque(found_ids)
-    print("Ready !")
+    print("Ready ")
 
 def interchangeId(ids, id1, id2):
     if (id1 == id2):
@@ -75,6 +79,7 @@ def interchangeId(ids, id1, id2):
     return ids
 
 def changeId(ids):
+    
     for i in range(len(ids)):
         idNotGiven = True
         initialisation(ids)
@@ -82,7 +87,9 @@ def changeId(ids):
             pos = dxl_io.get_present_position(ids)
             for x in range(len(pos)):
                 if (abs(pos[x]) > 20):
-                    ids = interchangeId(ids, ids[x], i)
+                    print("Enter id for this motor :")
+                    tmp = int( input() )
+                    ids = interchangeId(ids, ids[x], tmp )
                     idNotGiven = False
                     print("Motor validated !")
                     raw_input('->')
@@ -108,17 +115,72 @@ def suivreSinusoide(ids, frequence,amplitude,temps):
 if __name__ == '__main__':
 
     # we first open the Dynamixel serial port
-    with pypot.dynamixel.DxlIO('/dev/ttyUSB0', baudrate=1000000) as dxl_io:
+##    with pypot.dynamixel.DxlIO('/dev/ttyUSB0', baudrate=1000000) as dxl_io:
+##
+##        # we can scan the motors
+##        found_ids = dxl_io.scan()  # this may take several seconds
+##     #   print 'Detected:', found_ids
+##      #  print('Do you want to change all the ids (1 to change 0 to not change) :')
+##
+##       # answer=input()
+##        #if int(answer) == 1:
+##        changeId(found_ids)
 
-        # we can scan the motors
-        found_ids = dxl_io.scan()  # this may take several seconds
-        print 'Detected:', found_ids
+          #  yesfound_ids = dxl_io.scan()  # this may take several seconds
+           # print 'Detected:', yesfound_ids
+        
+##        spider_robot=autodetect_robot()
+##        config = spider_robot.to_config()
+##        with open('spider_robot.json', 'wb') as f:
+##            json.dump(config, f)
+##        spider_robot.close()
 
+        
+           
+        spider_robot=from_json('spider_robot.json')
+        print "petit message"
+        for m in spider_robot.thirdMotors :
+            m.compliant = False
+            m.goal_speed = 150
+            m.goal_position = 0
+        for m in spider_robot.secondMotors :
+            m.compliant = False
+            m.goal_speed = 100
+            m.goal_position = 0
+        for m in spider_robot.firstMotors :
+            m.compliant = False
+            m.goal_speed = 100
+            m.goal_position = 0
+            
+        time.sleep(2)
+        t0 = time.time()
+        while (True):
+            t = math.sin(2 * math.pi * 0.5 * time.time() - t0) - 0.5
+            if (t < 0):
+                spider_robot.motor_12.goal_position =  -60 * abs(t)
+                if (abs(t) > 0.5):
+                    spider_robot.motor_13.goal_position = 150 * (abs(t) - 0.5)
+                time.sleep(0.02)
+            else:
+                spider_robot.motor_42.goal_position =  -60 * abs(t)
+                if (abs(t) > 0.5):
+                    spider_robot.motor_43.goal_position = 150 * (abs(t) - 0.5)
+                time.sleep(0.02)
+        
+
+        spider_robot.close()
+
+
+
+
+
+
+        
         # we power on the motors
-        dxl_io.enable_torque(found_ids)
+        #dxl_io.enable_torque(found_ids)
 
         # we get the current positions
-        print 'Current pos:', dxl_io.get_present_position(found_ids)
+        #print 'Current pos:', dxl_io.get_present_position(found_ids)
 
         # # we create a python dictionnary: {id0 : position0, id1 : position1...}
         # pos = dict(zip(found_ids, itertools.repeat(0)))
@@ -130,18 +192,18 @@ if __name__ == '__main__':
         #changeId(found_ids)
         #suivreSinusoide(found_ids,0.5,30,15)
         # we get the current positions
-        print 'New pos:', dxl_io.get_present_position(found_ids)
+       # print 'New pos:', dxl_io.get_present_position(found_ids)
 ##        dxl_io.disable_torque(found_ids)
-        pos = dict(zip(found_ids, [0,30,-67]))
-        dxl_io.set_goal_position(pos)
-        dxl_io.disable_torque(found_ids)
+        #pos = dict(zip(found_ids, [0,30,-67]))
+        #dxl_io.set_goal_position(pos)
+        #dxl_io.disable_torque(found_ids)
 ##        while True :
 ##                angle=dxl_io.get_present_position(found_ids)
 ##                print 'Position :',leg_dk(angle[0],angle[1],angle[2])
 ##                time.sleep(0.5)
             
         # we power off the motors
-
+        
         time.sleep(1)  # we wait for 1s
 
         
